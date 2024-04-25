3ee7a4349396773745959a5707bb5a674e2e40b9
Ibrahim Fidahoussen <ibrahimf021202@gmail.com>
2024-04-25 02:21:28 +0200

    systeme de login utlisant l'app django

--- projet_info/urls.py
+++ projet_info/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+
+    #Django auth trucs
+    path('/accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login')
 ]
+
+#path('accounts/', include('django.contrib.auth.urls'))
+
+#accounts/login/ [name='login']
+#accounts/logout/ [name='logout']
+#accounts/password_change/ [name='password_change']
+#accounts/password_change/done/ [name='password_change-done']
+#accounts/password_reset/ [name='password_reset']
+#accounts/password_reset/done/ [name='password_reset_done'
+#accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+#accounts/reset/done/ [name='password_reset_complete']

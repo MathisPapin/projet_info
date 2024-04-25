010852fb0c6c29d9668dab48a97317e3e1fe60b6
Ibrahim Fidahoussen <ibrahimf021202@gmail.com>
2024-04-25 03:49:10 +0200

    ajout d'une page login et utilisateur

--- projet_info/urls.py
+++ projet_info/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 
     #Django auth trucs
-    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login')
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout")
 ]
 
 #path('accounts/', include('django.contrib.auth.urls'))
